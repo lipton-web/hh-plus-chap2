9c9ace47c3ebb20dfd4446a3e1968df256eb43da
lipton-web <cpt.lipton@gmail.com>
2024-03-22 23:36:26 +0900

    assignment B

--- src/hooks.js
+++ src/hooks.js
@@ -1,15 +1,38 @@
 export function createHooks(callback) {
+  let states = [];
+  let calls = 0;
+  console.log("createHooks", callback);
+
   const useState = (initState) => {
-    return [];
+    console.log("initState", initState);
+    // return [];
+    if (states[calls]) {
+      let state = states[calls];
+      const setState = (newState) => {
+        console.log("newState", newState);
+        states[calls] = newState;
+        callback();
+      };
+      calls++;
+      return [state, setState];
+    } else {
+      let state = initState;
+      const setState = (newState) => {
+        console.log("newState2", newState);
+        states[calls] = newState;
+        callback();
+      };
+      states[calls] = state;
+      calls++;
+      return [state, setState];
+    }
   };
 
   const useMemo = (fn, refs) => {
     return fn();
   };
 
-  const resetContext = () => {
-
-  }
+  const resetContext = () => {};
 
   return { useState, useMemo, resetContext };
 }
